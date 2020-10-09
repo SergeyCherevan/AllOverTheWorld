0adc4346922260f4605fa860cf36487bd5e08b4c
Сергей Черевань <60434110+SergeyCherevan@users.noreply.github.com>
2020-10-09 17:38:01 +0300

    Adding php files

--- php/form-processing.php
+++ php/form-processing.php
@@ -1,1 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Первый сайт на PHP</title>
+	<meta charset="utf-8">
+</head>
 
+<body>
+	<?php
+		$name = $_POST["input-name"];
+		echo "Спасибо, ".$name . ", наш менеджер свяжется с вами в ближайшее время!";
+	?>
+</body>
+</html>
