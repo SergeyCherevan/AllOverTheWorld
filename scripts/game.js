660fc086f3d5a6671f74c5b999117e4b986c4f0c
Сергей Черевань <60434110+SergeyCherevan@users.noreply.github.com>
2020-10-09 17:47:27 +0300

    Illya's work

---
+++ scripts/game.js
@@ -0,0 +1,7 @@
+
+
+document.getElementById('img_1').addEventListener('click',Rigth_answer);
+function Rigth_answer() {
+    alert("Work");
+document.getElementsByClassName('speech')[0].innerHTML="Congratulation";
+}
